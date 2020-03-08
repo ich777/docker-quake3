c226d4ad6c70e66f64140d04cb749485c4bfda19
ich777 <ich777@noreply.example.org>
2020-03-08 22:35:21 +0100

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,6 +15,36 @@ else
 	echo "---No optional script found, continuing---"
 fi
 
+echo "---Checking if Quake3 is installed---"
+if [ ! -f ${DATA_DIR}/q3ded ]; then
+	cd ${DATA_DIR}
+	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/quake.run "${DL_URL_PR}" ; then
+    	echo "---Successfully downloaded Quake3---"
+	else
+    	echo "---Can't download Quake3 please check your download URL, putting server into sleep mode---"
+        sleep infinity
+	fi
+	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/quake.zip "${DL_URL_PATCH}" ; then
+    	echo "---Successfully downloaded Quake3 Patch---"
+	else
+    	echo "---Can't download Quake3 Patch please check your download URL, putting server into sleep mode---"
+        sleep infinity
+	fi
+    chmod +x ${DATA_DIR}/quake.run
+    ${DATA_DIR}/quake.run --target ${DATA_DIR}/quake3/install
+    unzip ${DATA_DIR}/quake.zip
+    rm quake.run
+    rm quake.zip
+    mkdir -p /tmp/quake3
+    mv ${DATA_DIR}/install/bin/Linux/x86/* /tmp/quake3/
+    mv ${DATA_DIR}/Quake*/linux/* /tmp/quake3/
+    rm -R ${DATA_DIR}/*
+    mv /tmp/quake3/* ${DATA_DIR}/
+    mkdir -p ${DATA_DIR}/.q3d/baseq3
+    chown -R ${UID}:${GID} ${DATA_DIR}
+    chmod -R ${DATA_PERM} ${DATA_DIR}    
+fi
+
 echo "---Starting...---"
 chown -R ${UID}:${GID} /opt/scripts
 su ${USER} -c "/opt/scripts/start-server.sh"
